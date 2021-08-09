3eb8bfea203480f40cb43143b6682292b80c805f
Tom Whitwell <tom@whi.tw>
2021-08-09 12:15:28 +0100

    Update firmware to match e3d settings
    from: https://e3d-online.dozuki.com/Guide/Titan+Aero+Marlin+Configuration/39

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -557,7 +557,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 285
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
@@ -920,7 +920,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 837 }
 
 /**
  * Default Max Feed Rate (mm/s)
